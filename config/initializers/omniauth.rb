d2ece66aeeab66b3f531160029be608f63e1aa66
Jessica <jessica.weeber@gmail.com>
2016-05-17 09:03:20 -0700

    omniauth file

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+require 'rspotify/oauth'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :spotify, "SPOTIFY_CLIENT_ID", "SPOTIFY_CLIENT_SECRET", scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
+end
