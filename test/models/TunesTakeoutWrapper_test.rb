62698ee6089de3aa2e16dde5867855f80469c35c
Jessica <jessica.weeber@gmail.com>
2016-05-22 11:48:13 -0700

    working on wrapper testing. lookin good

--- test/models/TunesTakeoutWrapper_test.rb
+++ test/models/TunesTakeoutWrapper_test.rb
@@ -1,27 +1,40 @@
 require 'test_helper'
-require 'TunesTakeoutWrapper'
+require "TunesTakeoutWrapper"
 
 class TunesTakeoutWrapperTest < ActiveSupport::TestCase
 
-describe TunesTakeoutWrapper do
+  describe TunesTakeoutWrapper do
 
-  it "uses v1 of the API" do
-    assert_equal "https://tunes-takeout-api.herokuapp.com/v1", TunesTakeoutWrapper::BASE_URL
+    it "uses v1 of the API" do
+      assert_equal "https://tunes-takeout-api.herokuapp.com/v1", TunesTakeoutWrapper::BASE_URL
+    end
+
+    describe "API", :vcr do
+      before do
+        @suggestion_id = TunesTakeoutWrapper.get_suggestion("Vz0KO4-RRwADbn9c")
+        @top_suggestions = TunesTakeoutWrapper.top_suggestions
+        @search = TunesTakeoutWrapper.search("honey")
+      end
+
+      it "can retrieve a hash of suggestion info given a suggestion id", :vcr do
+        assert_kind_of Hash, @suggestion_id
+      end
+
+      it "can retrieve top 20 suggestions", :vcr do
+        assert_equal 20, @top_suggestions.length
+      end
+
+      it "can return 20 suggestions after user search", :vcr do
+        assert_equal 20, @search.length
+      end
+
+
+
+
+
+    end
+
+
   end
-
-  # describe "API" do
-  #   before do
-  #     results = {"id"=>"Vz0KNY-RRwADbn2k", "food_id"=>"ohana-seattle-2", "music_id"=>"0DeXBSPxuMGmgT1MSrzXe2", "music_type"=>"album"}
-  #     @banana_album = Music.get_music(results)
-  #   end
-
-  #   it "can find an album name", :vcr do
-  #     assert_equal "Banana Slug String Band", @banana_album.name
-  #   end
-
-  # end
-
-
-end
 
 end
