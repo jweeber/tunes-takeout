4d7d066bb2cf70c695edbdea290ef3575fd05213
Jessica <jessica.weeber@gmail.com>
2016-05-23 12:22:37 -0700

    migration

--- db/migrate/20160517231850_add_user_columns.rb
+++
@@ -1,7 +0,0 @@
-class AddUserColumns < ActiveRecord::Migration
-  def change
-  	add_column :users, :provider, :string, null: false
-  	add_column :users, :uid, :string, null: false
-  	add_column :users, :name, :string
-  end
-end
